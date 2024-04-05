8226d45d54a3a7a8abe57ab176cb5d8db7104329
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-04-05 17:55:42 -0300

    feat: create getRepositoryDetailsById

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ import TextIntro from './components/TextTitle';
 
 function App() { 
   const [theme, setTheme] = useState(darkTheme);
+
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === lightTheme ? darkTheme : lightTheme)); 
   };  
- 
+
   async function searchForTermOnAPI(searchTerm: string) {
     const escapedSearchTerm = JSON.stringify(searchTerm);
     const query = `
@@ -47,8 +48,67 @@ function App() {
     console.log('Data:', data.search.edges); 
   }
 
+
+  async function getRepositoryDetailsById(repoName?: string, repoUser?: string) {
+    // const formatterRepoName = JSON.stringify(repoName);
+    // const formatterRepoUser = JSON.stringify(repoUser); 
+
+     const query = `{
+      repository(name: "react", owner: "facebook") {
+        name
+        description
+        owner {
+          login
+        }
+        createdAt
+        updatedAt
+        stargazerCount
+        watchers {
+          totalCount
+        }
+        forks {
+          totalCount
+        }
+        languages(first: 5) {
+          nodes {
+            name
+          }
+        }
+        issues {
+          totalCount
+        }
+        pullRequests {
+          totalCount
+        }
+        defaultBranchRef {
+          target {
+            ... on Commit {
+              history {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+      const res = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          Authorization: `bearer ${process.env.REACT_APP_GIT_API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query})
+      });
+
+      const data = await res.json();
+      console.log('Data:', data); 
+  }
+  
+
   const handleSearch = (searchTerm: string) => { 
-    searchForTermOnAPI(searchTerm)
+    //  searchForTermOnAPI(searchTerm)  
+     getRepositoryDetailsById()
   };
   return (
     <ThemeProvider theme={theme}>  
