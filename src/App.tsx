41d76b1523c23259bcb3fafd5b2f5cf163d014ae
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-04-05 18:06:30 -0300

    refactor: move graphql services to a separate folder

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ToggleButton } from './styles/styles';
 import { SearchBar } from './components/SearchBar';
 import HomePage from './pages';
 import TextIntro from './components/TextTitle'; 
+import { getRepositoryDetailsById, searchForTermOnAPI } from './services';
 
 
 function App() { 
@@ -12,98 +13,10 @@ function App() {
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === lightTheme ? darkTheme : lightTheme)); 
-  };  
-
-  async function searchForTermOnAPI(searchTerm: string) { 
-    const query = `
-    query {
-      search(query: "${searchTerm}", type: REPOSITORY, first: 10) {
-        edges {
-          node {
-            ... on Repository {
-              id
-              name
-              description
-              owner {
-                login
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
-
-    const res = await fetch("https://api.github.com/graphql", {
-      method: "POST",
-      headers: {
-        Authorization: `bearer ${process.env.REACT_APP_GIT_API_KEY}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ query})
-    }); 
-    const { data } = await res.json();
-    // console.log('restpo', res)
-    console.log('Data:', data.search.edges); 
-  }
-
-
-  async function getRepositoryDetailsById(repoName: string, repoUser: string) { 
-     const query = `{
-      repository(name: "${repoName}", owner: "${repoUser}") {
-        name
-        description
-        owner {
-          login
-        }
-        createdAt
-        updatedAt
-        stargazerCount
-        watchers {
-          totalCount
-        }
-        forks {
-          totalCount
-        }
-        languages(first: 5) {
-          nodes {
-            name
-          }
-        }
-        issues {
-          totalCount
-        }
-        pullRequests {
-          totalCount
-        }
-        defaultBranchRef {
-          target {
-            ... on Commit {
-              history {
-                totalCount
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
-      const res = await fetch("https://api.github.com/graphql", {
-        method: "POST",
-        headers: {
-          Authorization: `bearer ${process.env.REACT_APP_GIT_API_KEY}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ query})
-      });
-
-      const data = await res.json();
-      console.log('Data:', data); 
-  }
-  
+  };     
 
   const handleSearch = (searchTerm: string) => { 
-    // searchForTermOnAPI(searchTerm)  
+    searchForTermOnAPI(searchTerm)  
     // getRepositoryDetailsById('react', 'facebook')
   };
   return (
---
+++ src/services/index.ts
@@ -0,0 +1,86 @@
+export async function searchForTermOnAPI(searchTerm: string) { 
+    const query = `
+    query {
+      search(query: "${searchTerm}", type: REPOSITORY, first: 10) {
+        edges {
+          node {
+            ... on Repository {
+              id
+              name
+              description
+              owner {
+                login
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+    const res = await fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        Authorization: `bearer ${process.env.REACT_APP_GIT_API_KEY}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ query})
+    }); 
+    const { data } = await res.json();
+    // console.log('restpo', res)
+    console.log('Data:', data.search.edges); 
+  }
+
+
+export async function getRepositoryDetailsById(repoName: string, repoUser: string) { 
+     const query = `{
+      repository(name: "${repoName}", owner: "${repoUser}") {
+        name
+        description
+        owner {
+          login
+        }
+        createdAt
+        updatedAt
+        stargazerCount
+        watchers {
+          totalCount
+        }
+        forks {
+          totalCount
+        }
+        languages(first: 5) {
+          nodes {
+            name
+          }
+        }
+        issues {
+          totalCount
+        }
+        pullRequests {
+          totalCount
+        }
+        defaultBranchRef {
+          target {
+            ... on Commit {
+              history {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+      const res = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          Authorization: `bearer ${process.env.REACT_APP_GIT_API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query})
+      });
+
+      const data = await res.json();
+      console.log('Data:', data); 
+  }
