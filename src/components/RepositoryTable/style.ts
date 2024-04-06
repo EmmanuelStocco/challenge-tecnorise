a6b6c81eeddd272d504557304ad10502c6f41198
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-04-06 02:12:56 -0300

    feat: styled to RepositoryTable

--- src/components/RepositoryTable/style.ts
+++ src/components/RepositoryTable/style.ts
@@ -1,23 +1,25 @@
 import styled from 'styled-components';
 
-
-
 export const TableContainer = styled.table`
   width: 60%;
-  border-collapse: collapse; 
+  border-collapse: separate;
+  border-spacing: 0 1px; /* Espaçamento entre as linhas */
   margin-top: 2rem;
   background-color: ${(props) => props.theme.colors.container};
 `;
 
 export const TableHead = styled.thead`
-  background-color: ${(props) => props.theme.colors.container};
+  background-color: ${(props) => props.theme.colors.dark};
 `;
 
 export const TableHeadRow = styled.tr``;
 
 export const TableHeadCell = styled.th`
   padding: 8px;
-  text-align: left;
+  text-align: center;
+  font-size: 1.5rem;
+  border-bottom: 0.5px solid ${(props) => props.theme.colors.text};
+  border-right: 0.5px solid ${(props) => props.theme.colors.text}; /* Linha entre as colunas */
 `;
 
 export const TableBody = styled.tbody``;
@@ -31,4 +33,8 @@ export const TableBodyRow = styled.tr`
 
 export const TableBodyCell = styled.td`
   padding: 8px;
+  font-size: 1.2rem;
+  text-align: center;
+  border-bottom: 1px solid ${(props) => props.theme.colors.text};
+  border-right: 1px solid ${(props) => props.theme.colors.text}; /* Linha entre as colunas */
 `;
