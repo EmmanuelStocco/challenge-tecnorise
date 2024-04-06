f817de00fd099a055a572aa5f49d664d10be1485
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-04-06 02:20:33 -0300

    feat: create toggleDarkThemeCurrent logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-import { ToggleButton } from './styles/styles';
+import { ThemeProvider } from 'styled-components'; 
 import { lightTheme, darkTheme } from './styles/theme'; 
 import HomePage from './pages'; 
 import store from './store';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 
 function App() { 
   const [theme, setTheme] = useState(darkTheme);
+  const [darkThemeCurrent, setDarkThemeCurrent] = useState<boolean>(true);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === lightTheme ? darkTheme : lightTheme)); 
+    setDarkThemeCurrent(!darkThemeCurrent)
   };   
   return (
     <Provider store={store}>
     <ThemeProvider theme={theme}>  
-      <ToggleButton onClick={toggleTheme}>Toggle Theme</ToggleButton>  
+      {/* <ToggleButton onClick={toggleTheme}>Toggle Theme</ToggleButton>   */}
+      <DarkModeSwitch
+      style={{  position: 'absolute', right:0, padding: 8}}
+      checked={darkThemeCurrent}
+      onChange={toggleTheme}
+      size={100}
+    />
        <HomePage />
     </ThemeProvider>
     </Provider>
