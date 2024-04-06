729f13bcb3aee05ff50ae6a0506956982883278f
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-04-05 22:18:56 -0300

    feat: pass searchTerm to rootSaga

--- src/store/ducks/rootSaga.ts
+++ src/store/ducks/rootSaga.ts
@@ -1,10 +1,20 @@
 import { all, takeLatest } from 'redux-saga/effects';
+import { ActionType } from 'typesafe-actions';
 
 import { RepositoriesTypes } from './repositories/types'
 import { load } from './repositories/sagas'
+import { loadRequest } from './repositories/actions'; // Importe a action LOAD_REQUEST
 
+// Crie uma interface para representar a action LOAD_REQUEST
+interface LoadRequestAction extends ActionType<typeof loadRequest> {
+    payload: {
+        searchTerm: string;
+    };
+}
+
+// Use a ActionType para inferir o tipo correto da action
 export default function* rootSaga(): Generator {
     return yield all([
-        takeLatest(RepositoriesTypes.LOAD_REQUEST, (action) => load(String(action))),
+        takeLatest<LoadRequestAction>(RepositoriesTypes.LOAD_REQUEST, (action) => load(action.payload.searchTerm)),
     ]);
 }
